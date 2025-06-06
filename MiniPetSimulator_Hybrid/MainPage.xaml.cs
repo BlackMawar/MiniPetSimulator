f062f0395e926ce087ab910f2d414b12e0690e09
AnifRastari <anif.rastari@gmail.com>
2025-06-06 15:23:38 +0800

    Blazor_Integration
    MiniPetsimulator_Hybrid

---
+++ MiniPetSimulator_Hybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniPetSimulator_Hybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MiniPetSimulator_Hybrid/Models/Pet.cs
@@ -0,0 +1,21 @@
+﻿namespace MiniPetSimulator_Hybrid.Models
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+
+        public Pet(string name)
+        {
+            Name = name;
+        }
+
+        public virtual string Speak()
+        {
+            return $"{Name} make a sound";
+        }
+        public string Eat()
+        {
+            return $"{Name} is eating...";
+        }
+    }
+}
