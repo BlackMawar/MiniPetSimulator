d9c60e847a142b8ea887729baf37a8b535691669
AnifRastari <anif.rastari@gmail.com>
2025-06-06 14:57:30 +0800

    blazor integration

---
+++ MiniPetSimulator_Hybrid/Models/Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniPetSimulator_Hybrid.Models
+{
+    internal class Cat : Pet
+    {
+        public Cat(string name) : base(name) { }
+        public override string Speak()
+        {
+            return $"{Name} says: Meow Meow!";
+        }
+    }
+}
---
+++ MiniPetSimulator_Hybrid/Models/Dog.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniPetSimulator_Hybrid.Models { 
+    public class Dog : Pet
+    {
+        public Dog(string name) : base(name) { }
+        public override string Speak()
+        {
+            return $"{Name} says: woof woof!";
+        }
+    }
+}
